5a4db197129266e515152cd45ebce42274ef59aa
TasnimJubaier <tasnimjubaier@gmail.com>
2023-11-11 17:35:22 +0600

    Update switch button logic

--- POS/Views/Shell.xaml.cs
+++ POS/Views/Shell.xaml.cs
@@ -24,8 +24,47 @@ namespace POS.Views
         public Shell()
         {
             InitializeComponent();
-
             this.DataContext = new ShellViewModel(this);
+        }
+
+        public void SwitchButton(string button)
+        {
+            ResetButtons();
+            switch (button)
+            {
+                case "Purchase":
+                    B1.Background = new SolidColorBrush(Color.FromRgb(0x2C, 0x4C, 0x61));
+                    break;
+                case "Employee":
+                    B2.Background = new SolidColorBrush(Color.FromRgb(0x2C, 0x4C, 0x61));
+                    break;
+                case "Bills":
+                    B3.Background = new SolidColorBrush(Color.FromRgb(0x2C, 0x4C, 0x61));
+                    break;
+                case "Customers":
+                    B4.Background = new SolidColorBrush(Color.FromRgb(0x2C, 0x4C, 0x61));
+                    break;
+                case "Products":
+                    B5.Background = new SolidColorBrush(Color.FromRgb(0x2C, 0x4C, 0x61));
+                    break;
+                case "Stock":
+                    B6.Background = new SolidColorBrush(Color.FromRgb(0x2C, 0x4C, 0x61));
+                    break;
+                case "BillCollection":
+                    B7.Background = new SolidColorBrush(Color.FromRgb(0x2C, 0x4C, 0x61));
+                    break;
+            }
+        }
+
+        public void ResetButtons()
+        {
+            B1.Background = new SolidColorBrush(Color.FromRgb(0x4D, 0x89, 0xB0));
+            B2.Background = new SolidColorBrush(Color.FromRgb(0x4D, 0x89, 0xB0));
+            B3.Background = new SolidColorBrush(Color.FromRgb(0x4D, 0x89, 0xB0));
+            B4.Background = new SolidColorBrush(Color.FromRgb(0x4D, 0x89, 0xB0));
+            B5.Background = new SolidColorBrush(Color.FromRgb(0x4D, 0x89, 0xB0));
+            B6.Background = new SolidColorBrush(Color.FromRgb(0x4D, 0x89, 0xB0));
+            B7.Background = new SolidColorBrush(Color.FromRgb(0x4D, 0x89, 0xB0));
         }
     }
 }
