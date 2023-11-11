d3de195fafad24ec51794ef003bd9ba24f0f5cf2
TasnimJubaier <tasnimjubaier@gmail.com>
2023-11-11 21:29:59 +0600

    Add bill view

---
+++ POS/Views/BillView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POS.Views
+{
+    /// <summary>
+    /// Interaction logic for BillView.xaml
+    /// </summary>
+    public partial class BillView : UserControl
+    {
+        public BillView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
